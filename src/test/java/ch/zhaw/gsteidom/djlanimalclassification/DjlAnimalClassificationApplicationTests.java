a76ab4f8ceeea33ed1c9bdc0c1d09498e060e2cc
GsteigerDominik <dominik.gsteiger@gmail.com>
2024-04-23 12:17:36 +0200

    Initial Spring Setup

---
+++ src/test/java/ch/zhaw/gsteidom/djlanimalclassification/DjlAnimalClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.gsteidom.djlanimalclassification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjlAnimalClassificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
